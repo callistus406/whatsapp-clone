feda955f50d91045540c3f17435210c53a1a182a
callistus406 <callistus406@gmail.com>
2022-06-05 15:23:30 +0100

    added login controller

--- server/Api/Controllers/loginController.js
+++ server/Api/Controllers/loginController.js
@@ -1,16 +1,2 @@
-const User = require("../../Model/user");
-
-const loginController = async (req, res) => {
-  try {
-    console.log(req.body);
-    const newUser = new User({
-      username: req.body.username,
-      phone: req.body.phone,
-    });
-    await newUser.save();
-    res.status(200).json(req.body);
-  } catch (err) {
-    res.status(200).json(err.message);
-  }
-};
+const loginController = async (req, res) => {};
 module.exports = loginController;
