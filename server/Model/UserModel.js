944b9349d8184701d8cd26042d2a9c602bb66c83
callistus406 <callistus406@gmail.com>
2022-06-05 15:22:05 +0100

    added user model

---
+++ server/Model/UserModel.js
@@ -0,0 +1,27 @@
+const { mongoose, Schema } = require("./getMongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      maxLength: 20,
+      minLength: 3,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      maxLength: 12,
+      minLength: 10,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+    },
+    about: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
