5df671e1a06b356054982e670bc926b2b20f34dd
callistus406 <callistus406@gmail.com>
2022-06-05 15:24:30 +0100

    added conversation controller

--- server/Api/Controllers/conversationController.js
+++ server/Api/Controllers/conversationController.js
@@ -1,8 +1,12 @@
-const conversationModel = require("../../Model/conversationModel");
+const ConversationModel = require("../../Model/ConversationModel");
 
 const conversationController = async (req, res) => {
   const { member, channelId, read, numberOfMsgs, visibility } = req.body;
   try {
+    // const newConversation = new Conversation({
+
+    // });
+    console.log("still in progress");
   } catch (error) {}
 };
 
