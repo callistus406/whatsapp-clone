da01b7d354a7534b75505ee4efa66625d67e6f86
callistus406 <callistus406@gmail.com>
2022-06-05 15:21:36 +0100

    added message model

---
+++ server/Model/MessageModel.js
@@ -0,0 +1,18 @@
+const { mongoose, Schema } = require("./getMongoose");
+
+const MessagesSchema = new Schema(
+  {
+    conversationId: {
+      type: String,
+    },
+    sender: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", MessagesSchema);
