c01e4104e279ee185f8ebe640c43d2a0e3746926
callistus406 <callistus406@gmail.com>
2022-06-09 21:27:34 +0100

    modified

---
+++ server/Api/Controllers/messageController.js
@@ -0,0 +1,36 @@
+const MessageModel = require("../../Model/MessageModel");
+
+const postMessageController = async (req, res) => {
+  const { conversationId, sender, text } = req.body;
+  console.log(req.body);
+  const newMessage = new MessageModel({
+    conversationId,
+    sender,
+    text,
+  });
+
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (error) {
+    res.status(200).json(error);
+  }
+};
+
+const getMessageController = async (req, res) => {
+  try {
+    console.log(req.params);
+    const messages = await MessageModel.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(200).json(error);
+  }
+};
+
+module.exports = {
+  postMessageController,
+  getMessageController,
+};
+// module.exports = ;
