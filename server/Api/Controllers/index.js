8a47eaaf7ed096e45c5df9810f95dcbae7eba2aa
callistus406 <callistus406@gmail.com>
2022-06-09 21:20:24 +0100

    added messageController, registerCOntroller

--- server/Api/Controllers/index.js
+++ server/Api/Controllers/index.js
@@ -1,10 +1,18 @@
-// const registerController = require("./registerUser");
+const registerController = require("./registerController");
 const loginController = require("./loginController");
 // const profileController = require("./profileController");
 const conversationController = require("./conversationController");
+const {
+  postMessageController,
+  getMessageController,
+} = require("./messageController");
 module.exports = {
   conversationController,
   loginController,
+  registerController,
+  postMessageController,
+  getMessageController,
+
   //   profileController,
   //   deleteController,
 };
