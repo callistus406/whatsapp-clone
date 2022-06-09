5b63fa2af7ea9c59cfdfb475bac8457d8638b1d1
callistus406 <callistus406@gmail.com>
2022-06-09 21:24:04 +0100

    added message route

--- server/Api/index.js
+++ server/Api/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const login = require("./Routes/login");
 const register = require("./Routes/register");
 const conversation = require("./Routes/conversation");
+const message = require("./Routes/message");
 
 router.use(login);
 router.use(register);
 router.use(conversation);
+router.use(message);
 
 module.exports = router;
