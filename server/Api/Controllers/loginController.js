9c2f47c61295b8a82d601bf349f73804ce819081
callistus406 <callistus406@gmail.com>
2022-06-15 20:30:32 +0100

    fix some bugs

--- server/Api/Controllers/loginController.js
+++ server/Api/Controllers/loginController.js
@@ -2,7 +2,7 @@ const UserModel = require("../../Model/UserModel");
 const loginController = async (req, res) => {
   try {
     const user = await UserModel.findOne({
-      _id: "629ccb139cc7b37b4630750d",
+      _id: "629f66f6cd16b099af3c7dda",
     });
     res.status(200).json(user);
   } catch (error) {
