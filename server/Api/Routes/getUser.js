83f7424ce7c01fc092ec204cee7fb90c4df23f36
callistus406 <callistus406@gmail.com>
2022-06-25 19:03:58 +0100

    renamed getUserController and getUser route

--- server/Api/Routes/getUser.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUserController } = require("../Controllers");
-
-router.get("/user", getUserController);
-module.exports = router;
