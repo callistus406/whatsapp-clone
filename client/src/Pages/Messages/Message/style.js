e32d95a3fbc44116029e016f5ba275a72ac1debd
callistus406 <callistus406@gmail.com>
2022-10-10 18:30:27 +0100

    new file

---
+++ client/src/Pages/Messages/Message/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const styledChat = styled.div`
+  .talk-bubble {
+    margin: 2.5rem 3rem 0 3rem;
+    cursor: pointer;
+    display: inline-block;
+    position: relative;
+    max-width: 24rem;
+    height: auto;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px;
+  }
+
+  .talktext {
+    padding: 0 0.4rem 0.3rem 0.4rem;
+
+    text-align: left;
+    line-height: 1.5em;
+  }
+`;
