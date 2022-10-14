6314953098fa6f0e1bc7a7c0c5e3763a597b07ed
callistus406 <callistus406@gmail.com>
2022-10-14 21:19:40 +0100

    modified socket emitter

--- socket/index.js
+++ socket/index.js
@@ -1,10 +1,33 @@
-const { Server } = require("socket.io");
-const io = new Server(8900, {
+// const { Server } = require('socket.io');
+// const io = new Server(8900, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//   },
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   io.emit('welcome', 'hello everyone this is from socket');
+// });
+
+const io = require('socket.io')(8900, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
   },
 });
+let users = [];
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  // io.emit('welcome', 'hello everyone this is from socket {message}');
+  //
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id);
+    io.emit('getUsers', users);
+  });
+  socket.on;
 });
