e40977ecb33427b2f308099939ee1b36f69778e9
callistus406 <callistus406@gmail.com>
2022-10-16 09:43:53 +0100

    modified

--- socket/index.js
+++ socket/index.js
@@ -17,27 +17,33 @@ const io = require('socket.io')(8900, {
 });
 let users = [];
 const addUser = (userId, socketId) => {
+  console.log(userId, socketId);
   !users.some((user) => user.userId === userId) &&
     users.push({ userId, socketId });
+  console.log(users);
 };
 const removeUser = (socketId) => {
   users.filter((user) => user.socketId !== socketId);
 };
 
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
+const getUser = (receiverId) => {
+  users.find((user) => console.log(user.userId === receiverId));
+  return users.find((user) => user.userId === receiverId);
 };
+
 io.on('connection', (socket) => {
   console.log('a user connected');
   // io.emit('welcome', 'hello everyone this is from socket {message}');
-  //
+  //get user id and socket id from socket
   socket.on('addUser', (userId) => {
     addUser(userId, socket.id);
     io.emit('getUsers', users);
   });
   //send and get
-  socket.on('sendMessage', (senderId, receiverId, text) => {
-    const user = getUser(userId);
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    console.log('................');
+    const user = getUser(receiverId);
+    console.log(users);
     io.to(user.socketId).emit('getMessage', {
       senderId,
       text,
