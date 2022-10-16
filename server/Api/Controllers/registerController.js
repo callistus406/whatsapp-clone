94d383e714057f651125af59d25aaa7f613326eb
callistus406 <callistus406@gmail.com>
2022-10-16 09:43:10 +0100

    added model store

--- server/Api/Controllers/registerController.js
+++ server/Api/Controllers/registerController.js
@@ -1,4 +1,5 @@
-const UserModel = require("../../Model/UserModel");
+const UserModel = require('../../Model/UserModel');
+const RefreshTokenModel = require('../../Model/RefreshTokenModel');
 const registerController = async (req, res) => {
   const { username, phone, about } = req.body;
   console.log(req.body);
@@ -10,7 +11,17 @@ const registerController = async (req, res) => {
   try {
     // save user to mongodb
     const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
+
+    // sends response to the frontend
+    const newTokenStore = new RefreshTokenModel({
+      user_id: savedUser._id,
+    });
+    const createTokenStore = await newTokenStore.save();
+    res.status(200).json({
+      success: true,
+      payload: { createTokenStore, savedUser, createTokenStore },
+    });
+    // res.status(200).json(savedUser);
   } catch (err) {
     res.status(500).json(err.message);
   }
