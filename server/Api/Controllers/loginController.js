4f51ccfb653a61b56885d672dd145a3d6ddfdaea
callistus406 <callistus406@gmail.com>
2022-10-16 09:41:55 +0100

    made some changes

--- server/Api/Controllers/loginController.js
+++ server/Api/Controllers/loginController.js
@@ -1,16 +1,57 @@
 const UserModel = require('../../Model/UserModel');
+const jwt = require('jsonwebtoken');
+const RefreshTokenModel = require('../../Model/RefreshTokenModel');
+require('dotenv').config();
 const loginController = async (req, res) => {
   try {
     const { username, phone } = req.body;
     // logged in as splunk
-    console.log(req.body);
     const user = await UserModel.findOne({
       username,
       phone,
     });
-    res.status(200).json({ success: true, user });
+    const accessToken = generateJwtAccessToken(user);
+    const refreshToken = generateJwtRefreshToken(user);
+    // console.log(refreshToken, accessToken);
+
+    const dbTokenStore = await RefreshTokenModel.findOne({
+      user_id: user._id,
+    });
+
+    console.log(dbTokenStore);
+
+    res.status(200).json({
+      success: true,
+      payload: { user, accessToken, refreshToken, dbTokenStore },
+    });
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message);
   }
 };
+
+const generateJwtAccessToken = (user) => {
+  // console.log(user);
+  return jwt.sign(
+    {
+      id: user._id,
+      user_type: user.username,
+      email: user.phone,
+    },
+    process.env.JWT_SECRETE,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+};
+const generateJwtRefreshToken = (user) => {
+  console.log(user);
+
+  return jwt.sign(
+    {
+      id: user._id,
+      user_type: user.username,
+      email: user.phone,
+    },
+    process.env.JWT_REFRESH_SECRETE
+  );
+};
+
 module.exports = loginController;
