ab201301853cb016fdbbaf15b19189dc0caf7a79
callistus406 <callistus406@gmail.com>
2022-10-17 21:33:21 +0100

    modified

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
-const connection = require("./DbConnection/connection");
-const routes = require("./Api");
+const cors = require('cors');
+require('dotenv').config();
+const connect = require('./db/connect');
+const routes = require('./Api');
 const port = process.env.PORT || 3000;
 // middleWares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/v1", routes);
-connection();
-app.listen(port, () => {
-  console.log(`app is listening on port ${port}`);
-});
+app.use('/api/v1', routes);
+
+const start = async (url) => {
+  try {
+    await connect(process.env.MONGO_URI);
+    console.log(`mongodb is connected`);
+    app.listen(port, () => {
+      console.log(`app is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
