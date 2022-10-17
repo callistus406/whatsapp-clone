83de6dfde9e885c335857a1773d73b348ebeab6e
callistus406 <callistus406@gmail.com>
2022-10-17 21:17:33 +0100

    asyncWrapper

---
+++ server/middleware/asyncWrapper.js
@@ -0,0 +1,12 @@
+const asyncWrapper = (callback) => {
+  return async (req, res, next) => {
+    try {
+      await callback(req, res, next);
+    } catch (error) {
+      // console.log(error);
+      next(error);
+    }
+  };
+};
+
+module.exports = asyncWrapper;
