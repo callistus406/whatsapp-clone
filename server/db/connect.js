2443e1c440bfffa21d5f834790f5ef9162964e3f
callistus406 <callistus406@gmail.com>
2022-10-17 21:33:57 +0100

    renamed

---
+++ server/db/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const mongoose = require('mongoose');
+// database connector
+const connectDb = (url) => {
+  return mongoose.connect(url);
+  console.log('mongdb is connected');
+};
+
+module.exports = connectDb;
