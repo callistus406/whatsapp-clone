7de912868dc1514b2007f53f5294db1d6521c593
callistus406 <callistus406@gmail.com>
2022-10-18 18:53:10 +0100

    fixed some bugs

--- server/app.js
+++ server/app.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 require('dotenv').config();
 const connect = require('./db/connect');
 const routes = require('./Api');
+const error404 = require('./middleware/error404');
+const errorHandler = require('./middleware/errorHandler');
+
 const port = process.env.PORT || 3000;
 // middleWares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1', routes);
-
+app.use(error404);
+app.use(errorHandler);
 const start = async (url) => {
   try {
     await connect(process.env.MONGO_URI);
@@ -22,3 +26,4 @@ const start = async (url) => {
     console.log(error.message);
   }
 };
+start();
