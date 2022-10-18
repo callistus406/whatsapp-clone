a534e8e8ea35950f87701670b8aa6677f102c849
callistus406 <callistus406@gmail.com>
2022-10-18 19:38:50 +0100

    activated auth middleware

---
+++ server/Auth/authenticateToken.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const authenticateToken = async (req, res, next) => {
+  // get token from header
+  const authHeader = req.headers.authorization;
+
+  let token = null;
+  // check if header starts with auth header
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+  }
+  //   check if token exists
+  if (!token)
+    return res.status(401).json({
+      success: false,
+      payload: 'You are not authorized to access this resource',
+    });
+
+  // verify token
+  jwt.verify(token, process.env.JWT_SECRETE, (error, result) => {
+    if (error)
+      return res.status(403).json({ success: false, payload: 'Invalid Token' });
+    req.user = result;
+    next();
+  });
+
+  // add a code to check if there is an auth header
+};
+
+module.exports = authenticateToken;
