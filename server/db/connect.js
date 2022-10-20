064f532c5dae9071bcad475eb04a756db9cd231e
callistus406 <callistus406@gmail.com>
2022-10-20 11:55:56 +0100

    modified

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // database connector
 const connectDb = (url) => {
+  console.log(url);
   return mongoose.connect(url);
   console.log('mongodb is connected');
 };
