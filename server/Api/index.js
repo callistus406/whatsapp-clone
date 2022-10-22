c7b120aeae91be31eaf0b53d661999dc453c5abc
callistus406 <callistus406@gmail.com>
2022-10-22 21:12:30 +0100

    modified

--- server/Api/index.js
+++ server/Api/index.js
@@ -1,17 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const login = require("./Routes/login");
-const register = require("./Routes/register");
-const conversation = require("./Routes/conversation");
-const message = require("./Routes/message");
-const profile = require("./Routes/profile");
-const user = require("./Routes/user");
-
+const login = require('./Routes/login');
+const register = require('./Routes/register');
+const conversation = require('./Routes/conversation');
+const message = require('./Routes/message');
+const profile = require('./Routes/profile');
+const user = require('./Routes/user');
+const refresh = require('./Routes/refresh');
 router.use(login);
 router.use(register);
 router.use(conversation);
 router.use(message);
 router.use(profile);
 router.use(user);
+router.use(refresh);
 
 module.exports = router;
