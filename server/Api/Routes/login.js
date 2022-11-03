a659a5ea20f2ef99750db0f699ec218614921aec
callistus406 <callistus406@gmail.com>
2022-11-03 23:11:20 +0100

    added passport

--- server/Api/Routes/login.js
+++ server/Api/Routes/login.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { loginController } = require('../Controllers');
+const passport = require('../../middleware/passport');
+// const loginController = require('../../app/loginController');
 
 router.post('/login', loginController);
 module.exports = router;
