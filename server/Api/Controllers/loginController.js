3063eeb50ba3adbac0dc2b6c9076f4503face59e
callistus406 <callistus406@gmail.com>
2022-11-03 23:10:51 +0100

    added passport

--- server/Api/Controllers/loginController.js
+++ server/Api/Controllers/loginController.js
@@ -1,37 +1,95 @@
 const UserModel = require('../../Model/UserModel');
 const jwt = require('jsonwebtoken');
 const RefreshTokenModel = require('../../Model/RefreshTokenModel');
+const passport = require('passport');
 require('dotenv').config();
-const loginController = async (req, res) => {
-  try {
-    const { username, phone } = req.body;
-    // logged in as splunk
-    const user = await UserModel.findOne({
-      username,
-      phone,
-    });
-    const accessToken = generateJwtAccessToken(user);
-    const refreshToken = generateJwtRefreshToken(user);
-    // console.log(user._id);
+const initializePassport = require('../../middleware/passportConfig');
 
-    const dbTokenStore = await RefreshTokenModel.findOneAndUpdate(
-      {
-        user_id: user._id,
-      },
-      { refreshToken: refreshToken },
-      { new: true, runValidators: true }
-    );
-
-    // console.log(dbTokenStore);
-
-    res.status(200).json({
-      success: true,
-      payload: { user, accessToken, refreshToken, dbTokenStore },
-    });
-  } catch (error) {
-    res.status(500).json(error.message);
-  }
+const loginController = async (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    // console.log(info);
+    if (err) throw err;
+    if (!user) res.send('No User Exists');
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.status(200).json(user);
+        // console.log(req.user);
+      });
+    }
+  })(req, res, next);
 };
+//   initializePassport(
+//     passport,
+//     (username) => {
+//       user = UserModel.findOne({ username: username });
+//       return user;
+//     },
+//     async (id) => {
+//       user = await UserModel.findOne({ _id: id });
+//       return user;
+//     }
+//   );
+// };
+
+//   router.post(
+//     '/login',
+//     function (req, res, next) {
+//         console.log('routes/user.js, login, req.body: ');
+
+//         console.log(req.body)
+//         next()
+//     },
+//     passport.authenticate('local'),
+//     (req, res) => {
+//         console.log('logged in', req.user);
+//         var userInfo = {
+//             username: req.user.username
+//         };
+//         res.send(userInfo);
+//     }
+// )
+// passport.authenticate('local', (err, user, info) => {
+//   if (err) throw err;
+//   if (!user)
+//     return res.status(400).json({ success: false, payload: 'no user found' });
+//   req.logIn(user, (err) => {
+//     if (err) throw err;
+//     res.status(200).json({ success: true, payload: user });
+//   });
+// })(req, res, next);
+// };
+
+// const loginController = async (req, res) => {
+//   try {
+//     const { username, phone } = req.body;
+//     // logged in as splunk
+//     const user = await UserModel.findOne({
+//       username,
+//       phone,
+//     });
+//     const accessToken = generateJwtAccessToken(user);
+//     const refreshToken = generateJwtRefreshToken(user);
+//     // console.log(user._id);
+
+//     const dbTokenStore = await RefreshTokenModel.findOneAndUpdate(
+//       {
+//         user_id: user._id,
+//       },
+//       { refreshToken: refreshToken },
+//       { new: true, runValidators: true }
+//     );
+
+//     // console.log(dbTokenStore);
+
+//     res.status(200).json({
+//       success: true,
+//       payload: { user, accessToken, refreshToken, dbTokenStore },
+//     });
+//   } catch (error) {
+//     res.status(500).json(error.message);
+//   }
+// };
 
 const generateJwtAccessToken = (user) => {
   // console.log(user._id);
@@ -125,3 +183,56 @@ const userRefreshToken = async (req, res) => {
 };
 
 module.exports = { loginController, userRefreshToken };
+
+/**
+ * make an
+ *
+ */
+
+//  const refreshToken = async (data) => {
+//     console.log(data ? data : props.authToken[1]);
+//     try {
+//       const response = await axios.post('/refresh', {
+//         token: props.authToken[1],
+//       });
+//       console.log(response);
+//       let currentDate = new Date();
+//       const decodedToken = jwtDecode(props.userInfo.payload.accessToken);
+//       console.log(decodedToken);
+
+//       // setToken({
+//       //   refresh: response.data.payload.refreshToken,
+//       //   access: response.data.payload.accessToken,
+//       // });
+//       setTimeout(() => {
+//         console.log(response.data.payload.refreshToken);
+//         props.getToken(
+//           response.data.payload.accessToken,
+//           response.data.payload.refreshToken
+//         );
+//         // refreshToken();
+//         console.log(props.authToken[1]);
+//         // axios.config.headers['authorization'] =
+//         //   'Bearer ' + response.data.payload.accessToken;
+//       }, 59000);
+//       // if (decodedToken && decodedToken.exp * 1000 < currentDate.getTime()) {
+//       //   // const data = await refreshToken();
+
+//       //   axios.config.headers['authorization'] =
+//       //     'Bearer ' + response.data.payload.accessToken;
+//       // }
+//       console.log(response.data.payload.accessToken);
+
+//       // setAppUser({
+//       //   response: response.data.payload,
+//       //   ...appUser,
+//       //   accessToken: response.data.payload.accessToken,
+//       //   refreshToken: response.data.payload.refreshToken,
+//       // });
+
+//       console.log();
+//       return response.data.payload;
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
