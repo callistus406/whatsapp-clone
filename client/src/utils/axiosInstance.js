9913984306d9265ae964c2147219b83a7ec56cf8
callistus406 <callistus406@gmail.com>
2022-11-04 19:40:35 +0100

    modified

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-const axiosJWT = axios.create({
-  //   baseURL: 'http://localhost:3000',
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3300/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+  // withCredentials: true,
 });
-export default axiosJWT;
+export default axiosInstance;
