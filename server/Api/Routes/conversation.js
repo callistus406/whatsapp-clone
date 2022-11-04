a68dd983139e15b7241a2fe4e7f256ca2068798a
callistus406 <callistus406@gmail.com>
2022-11-04 19:41:58 +0100

    modified

--- server/Api/Routes/conversation.js
+++ server/Api/Routes/conversation.js
@@ -1,19 +1,15 @@
 const router = require('express').Router();
 const authenticateToken = require('../../Auth/authenticateToken');
+const authCheck = require('../../middleware/authCheck');
 const {
   conversationController,
   getConversationController,
 } = require('../Controllers');
 
-router.post(
-  '/conversation',
-  authenticateToken,
-
-  conversationController
-);
+router.post('/conversation', authCheck, conversationController);
 router.get(
   '/conversation/:userId',
-  authenticateToken,
+
   getConversationController
 );
 module.exports = router;
