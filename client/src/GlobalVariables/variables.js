d3bc3e6fb855079f5d094b8c65697e79bcfec31a
callistus406 <callistus406@gmail.com>
2022-11-10 23:52:48 +0100

    modified

--- client/src/GlobalVariables/variables.js
+++ client/src/GlobalVariables/variables.js
@@ -1,33 +1,33 @@
 export const groupDialog = [
-  { id: 1, text: "Archive chat" },
-  { id: 2, text: "Mute notification" },
-  { id: 3, text: "Exit group" },
-  { id: 4, text: "Pin chat" },
-  { id: 5, text: "Mark as read" },
+  { id: 1, text: 'Archive chat' },
+  { id: 2, text: 'Mute notification' },
+  { id: 3, text: 'Exit group' },
+  { id: 4, text: 'Pin chat' },
+  { id: 5, text: 'Mark as unread' },
 ];
 export const groupContext = [
-  { id: 1, text: "Group info" },
-  { id: 2, text: "Select messages" },
-  { id: 3, text: "Mute notification" },
-  { id: 4, text: "Clear  messages" },
-  { id: 5, text: "Exit group" },
+  { id: 1, text: 'Group info' },
+  { id: 2, text: 'Select messages' },
+  { id: 3, text: 'Mute notification' },
+  { id: 4, text: 'Clear  messages' },
+  { id: 5, text: 'Exit group' },
 ];
 export const messageDialog = [
-  { id: 1, text: "Reply" },
-  { id: 2, text: "Reply privately" },
-  { id: 3, text: "Download" },
-  { id: 4, text: "Forward message" },
-  { id: 5, text: "Star message" },
-  { id: 6, text: "Delete message" },
-  { id: 7, text: " message +2348123435567" },
+  { id: 1, text: 'Reply' },
+  { id: 2, text: 'Reply privately' },
+  { id: 3, text: 'Download' },
+  { id: 4, text: 'Forward message' },
+  { id: 5, text: 'Star message' },
+  { id: 6, text: 'Delete message' },
+  { id: 7, text: ' message +2348123435567' },
 ];
 
 export const contactDialog = [
-  "Archive chat",
-  "Mute notification",
-  "Delete chat",
-  "Pin chat",
-  "Mark as read",
+  'Archive chat',
+  'Mute notification',
+  'Delete chat',
+  'Pin chat',
+  'Mark as read',
 ];
 
 // ...........................................................
@@ -35,107 +35,107 @@ export const contactDialog = [
 
 export const keyObj = [
   {
-    name: "mark as Unread",
-    keys: ["Ctrl", "Alt", "Shift", "U"],
+    name: 'mark as Unread',
+    keys: ['Ctrl', 'Alt', 'Shift', 'U'],
   },
   {
-    name: "Archive chat",
-    keys: ["Ctrl", "Alt", "Shift", "E"],
+    name: 'Archive chat',
+    keys: ['Ctrl', 'Alt', 'Shift', 'E'],
   },
   {
-    name: "pin chat",
-    keys: ["Ctrl", "Alt", "Shift", "P"],
+    name: 'pin chat',
+    keys: ['Ctrl', 'Alt', 'Shift', 'P'],
   },
   {
-    name: "Search chat",
-    keys: ["Ctrl", "Alt", "Shift", "F"],
+    name: 'Search chat',
+    keys: ['Ctrl', 'Alt', 'Shift', 'F'],
   },
   {
-    name: "Next chat",
-    keys: ["Ctrl", "Alt", "Tab"],
+    name: 'Next chat',
+    keys: ['Ctrl', 'Alt', 'Tab'],
   },
   {
-    name: "New group",
-    keys: ["Ctrl", "Alt", "Shift", "N"],
+    name: 'New group',
+    keys: ['Ctrl', 'Alt', 'Shift', 'N'],
   },
   {
-    name: "increased speed of Selected Voice Message",
-    keys: ["Ctrl", "."],
+    name: 'increased speed of Selected Voice Message',
+    keys: ['Ctrl', '.'],
   },
   {
-    name: "Settings",
-    keys: ["Ctrl", "Alt", ","],
+    name: 'Settings',
+    keys: ['Ctrl', 'Alt', ','],
   },
   {
-    name: "Git Panel",
-    keys: ["Ctrl", "Alt", "G"],
+    name: 'Git Panel',
+    keys: ['Ctrl', 'Alt', 'G'],
   },
 ];
 export const keyObjCol2 = [
   {
-    name: "Mute",
-    keys: ["Ctrl", "Alt", "Shift", "M"],
+    name: 'Mute',
+    keys: ['Ctrl', 'Alt', 'Shift', 'M'],
   },
   {
-    name: "Delete Chat",
-    keys: ["Ctrl", "Alt", "Backspace"],
+    name: 'Delete Chat',
+    keys: ['Ctrl', 'Alt', 'Backspace'],
   },
   {
-    name: "Search",
-    keys: ["Ctrl", "Alt", "/"],
+    name: 'Search',
+    keys: ['Ctrl', 'Alt', '/'],
   },
   {
-    name: "New chat",
-    keys: ["Ctrl", "Alt", "N"],
+    name: 'New chat',
+    keys: ['Ctrl', 'Alt', 'N'],
   },
   {
-    name: "Previous Chat",
-    keys: ["Ctrl", "Alt", "Shift", "Tab"],
+    name: 'Previous Chat',
+    keys: ['Ctrl', 'Alt', 'Shift', 'Tab'],
   },
   {
-    name: "Profile and about",
-    keys: ["Ctrl", "Alt", "P"],
+    name: 'Profile and about',
+    keys: ['Ctrl', 'Alt', 'P'],
   },
   {
-    name: "Decrease speed of Selected Voice Message",
-    keys: ["Shift", ","],
+    name: 'Decrease speed of Selected Voice Message',
+    keys: ['Shift', ','],
   },
   {
-    name: "Emoji Panel",
-    keys: ["Ctrl", "Alt", "E"],
+    name: 'Emoji Panel',
+    keys: ['Ctrl', 'Alt', 'E'],
   },
   {
-    name: "Sticker Panel",
-    keys: ["Ctrl", "Alt", "G"],
+    name: 'Sticker Panel',
+    keys: ['Ctrl', 'Alt', 'G'],
   },
 ];
 
 export const textObj = [
   {
-    name: "mark as Unread",
+    name: 'mark as Unread',
   },
   {
-    name: "Archive chat",
+    name: 'Archive chat',
   },
   {
-    name: "pin chat",
+    name: 'pin chat',
   },
   {
-    name: "search chat",
+    name: 'search chat',
   },
   {
-    name: "Next chat",
+    name: 'Next chat',
   },
   {
-    name: "New group",
+    name: 'New group',
   },
   {
-    name: "increased speed of Selected Voice Message",
+    name: 'increased speed of Selected Voice Message',
   },
   {
-    name: "Settings",
+    name: 'Settings',
   },
   {
-    name: "Git Panel",
+    name: 'Git Panel',
   },
 ];
