fb69a8363ddfb7042085d2dbd8f72f6f06962abe
callistus406 <callistus406@gmail.com>
2022-11-10 23:52:39 +0100

    modified

--- client/src/components/Chat/index.jsx
+++ client/src/components/Chat/index.jsx
@@ -7,11 +7,17 @@ import {
   StyledUserChatText,
   StyledCircle,
   StyledCaret,
+  StyledContextMenu4MsgSpace,
+  StyledContextMenu,
+  StyledContextMenuItem4MsgSpace,
+  StyledContextMenuItem,
+  StyledBox,
+  Avater,
 } from './styles.js';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Icons } from './icons';
-
+import { messageDialog, groupDialog } from '../../GlobalVariables/variables.js';
 const dummyData = [
   {
     name: 'Hackers league',
@@ -29,11 +35,32 @@ const dummyData = [
 
 function ChatLayout({ data, index }) {
   const [caret, setCaret] = useState(true);
+  const [contextMenu, setContextMenu] = React.useState(null);
+
   const toggleCaret = (bool) => {
     console.log(bool);
     setCaret(bool);
 
     // caretRef.current
+  };
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+    toggleCaret(true);
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX - 2,
+            mouseY: event.clientY - 4,
+          }
+        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+          // Other native context menus might behave different.
+          // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+          null
+    );
+  };
+  const handleClose = () => {
+    setContextMenu(null);
   };
   return (
     <StyledChatLayoutCont
@@ -47,7 +74,32 @@ function ChatLayout({ data, index }) {
             <PeopleAltIcon style={{ color: '#fff' }} />
           </StyledCircle>
         </div>
-
+        <StyledContextMenu
+          PaperProps={{
+            style: {
+              // maxHeight: ITEM_HEIGHT * 4.5,
+              height: '13rem',
+              minWidth: '14rem',
+            },
+          }}
+          open={contextMenu !== null}
+          onClose={handleClose}
+          anchorReference="anchorPosition"
+          autoFocus={false}
+          anchorPosition={
+            contextMenu !== null
+              ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
+              : undefined
+          }
+        >
+          {groupDialog.map((item) => {
+            return (
+              <StyledContextMenuItem onClick={handleClose}>
+                {item.text}
+              </StyledContextMenuItem>
+            );
+          })}
+        </StyledContextMenu>
         <StyledUserChatText>
           <h3>{data.name}</h3>
 
@@ -57,7 +109,7 @@ function ChatLayout({ data, index }) {
           </div>
           {/* <Messages /> */}
         </StyledUserChatText>
-        <StyledCaret hide={caret}>
+        <StyledCaret hide={caret} onClick={handleContextMenu}>
           <KeyboardArrowDownIcon />
         </StyledCaret>
       </StyledUserChatCont>
